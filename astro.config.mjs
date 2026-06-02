a618da412a4011b807e8543e5b5f53853c7a7ee2
RG4Pablo21 <gic438@gmail.com>
2026-06-01 19:21:22 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://rg4pablo21.github.io',
-  base: '/gta-vi-gt',
-
   vite: {
     plugins: [tailwindcss()]
   }
